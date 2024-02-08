69b7b2e71f0b5c02417a370b2e66ed3fafbe0068
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2024-02-08 17:57:06 +1000

    Update BinarySum.php

--- src/Basic/BinarySum.php
+++ src/Basic/BinarySum.php
@@ -4,11 +4,7 @@ namespace Sedalit\PhpChallenges\Basic\BinarySum;
 
 /**
      * Функция, которая принимает на вход любое количество бинарных чисел (в виде строк) и возвращает их сумму (в виде строки). 
-     * @param string $first 
-     * @param string $other
-     * @return string
 */
-
 function binarySum(string $first, string $second) : string
 {
     $first = bindec($first);
