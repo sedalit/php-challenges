73212952c9846793c1621ef0617e02ca28615ecf
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2024-03-10 15:20:54 +1000

    Update FindIndexOfNearest.php

--- src/Functions/FindIndexOfNearest.php
+++ src/Functions/FindIndexOfNearest.php
@@ -6,16 +6,18 @@ namespace Sedalit\PhpChallenges\Functions\FindIndexOfNearest;
  * Функция, которая принимает на вход массив чисел и искомое число. Задача функции — найти в массиве ближайшее число к искомому и вернуть его индекс в массиве.
  * Если в массиве содержится несколько чисел, одновременно являющихся ближайшими к искомому числу, то возвращается наименьший из индексов ближайших чисел.
 */
-function findIndexOfNearest(array $numbers, float $targetNumber) : float
+function findIndexOfNearest(array $numbers, float $targetNumber) : int
 {
     if (count($numbers) === 0) return -1;
 
-    $index = 0;
-    array_reduce($numbers, function(int $acc, int $number) use ($targetNumber, $index) {
+    $result = array_reduce($numbers, function(array $acc, int $number) use ($targetNumber) {
         $difference = abs($number - $targetNumber);
-        $index++;
-        return $difference < $acc ? $difference : $acc;
-    }, $numbers[0]);
 
-    return $index;
+        $index = $acc['index'];
+        $value = $acc['value'];
+
+        return $difference < $value ? ['index' => $index + 1, 'value' => $difference] : $acc;
+    }, ['index' => 0, 'value' => $numbers[0]]);
+
+    return $result['index'];
 }
