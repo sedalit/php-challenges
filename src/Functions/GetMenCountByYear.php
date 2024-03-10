0a514f26d9f2e460b211d16adde4ddc6669cb42d
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2024-03-10 13:29:44 +1000

    Update GetMenCountByYear.php

--- src/Functions/GetMenCountByYear.php
+++ src/Functions/GetMenCountByYear.php
@@ -6,9 +6,7 @@ namespace Sedalit\PhpChallenges\Functions\GetMenCountByYear;
  * Функция, которая принимает на вход список пользователей и возвращает массив, в котором ключ это год рождения, а значение это количество мужчин, родившихся в этот год.
  */
 function getMenCountByYear(array $users) : array
-{
-    if (count($users) === 0) return [];
-    
+{    
     return array_reduce($users, function(array $acc, array $user) {
         if ($user['gender'] != "male") return $acc;
 
