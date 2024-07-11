755f70bb994813c0ad2ccf1cd158d3ff9f1a24ba
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2024-07-11 19:17:54 +1000

    Update UserFunctions.php

--- src/Classes/UserFunctions.php
+++ src/Classes/UserFunctions.php
@@ -4,11 +4,13 @@ namespace App\Classes\UserFunctions;
 
 use App\Classes\User;
 
+/**
+ * Функцию, которая сравнивает переданных пользователей на основе значений их идентификаторов.
+ * @param \App\Classes\User $firstUser
+ * @param \App\Classes\User $secondUser
+ * @return bool
+ */
 function areUsersEqual(User $firstUser, User $secondUser) : bool
 {
     return $firstUser == $secondUser;
 }
-
-//$firstUser = new User(12);
-
-//areUsersEqual($firstUser, $firstUser);
