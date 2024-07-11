8137afbbcb9b814fc32b54f569b6d1e448372439
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2024-07-11 18:54:40 +1000

    Add duplicate challenge

--- src/Classes/PointFunctions.php
+++ src/Classes/PointFunctions.php
@@ -17,3 +17,14 @@ function getMidpoint(Point $firstPoint, Point $secondPoint) : Point
         ($firstPoint->getY() + $secondPoint->getY()) / 2,
     );
 }
+
+/**
+ * Функция, которая клонирует переданную точку. 
+ * Под клонированием подразумевается процесс создания нового объекта, с такими же данными как и у старого.
+ * @param \App\Classes\Point $point
+ * @return \App\Classes\Point
+ */
+function duplicate(Point $point) : Point
+{
+    return new Point($point->getX(), $point->getY());
+}
--- tests/Classes/PointTest.php
+++ tests/Classes/PointTest.php
@@ -5,6 +5,7 @@ namespace Tests\Classes;
 use PHPUnit\Framework\TestCase;
 use App\Classes\Point;
 use function App\Classes\PointFunctions\getMidpoint;
+use function App\Classes\PointFunctions\duplicate;
 
 class PointTest extends TestCase {
     public function testGetMidpoint() : void
@@ -15,6 +16,17 @@ class PointTest extends TestCase {
         $result = getMidpoint($firstPoint, $secondPoint);
 
         $expected = new Point(5.5, 5.5);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testDuplicate() : void
+    {
+        $point = new Point(10, 1);
+
+        $result = duplicate($point);
+
+        $expected = new Point(10, 1);
 
         $this->assertEquals($expected, $result);
     }
